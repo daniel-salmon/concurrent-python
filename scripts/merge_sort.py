65580e1e0dea7fe56745c15459090a6e528e8585
daniel salmon <salmond44@gmail.com>
2024-12-22 15:54:03 -0500

    Add threaded merge sort implementation

--- scripts/merge_sort.py
+++ scripts/merge_sort.py
@@ -1,3 +1,9 @@
+from concurrent.futures import ThreadPoolExecutor
+from itertools import batched
+
+MAX_WORKERS = 6
+
+
 def merge_sort(a: list[int]) -> list[int]:
     if len(a) <= 1:
         return a
@@ -18,3 +24,35 @@ def merge(left: list[int], right: list[int]) -> list[int]:
     out.extend(left[lidx:])
     out.extend(right[ridx:])
     return out
+
+
+def threaded_merge_sort(a: list[int], max_workers: int = MAX_WORKERS) -> list[int]:
+    if len(a) <= 1:
+        return a
+
+    todo = [[x] for x in a]
+    if len(todo) % 2 == 1:
+        new_last = sorted([todo[-2][0], todo[-1][0]])
+        todo.pop()
+        todo[-1] = new_last
+
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        while len(todo) > 1:
+            futures = []
+            for pair in batched(todo, n=2):
+                futures.append(executor.submit(merge, pair[0], pair[1]))
+
+            todo = []
+            for future in futures:
+                todo.append(future.result())
+
+    return todo[0]
+
+
+if __name__ == "__main__":
+    x = [5, 4, 3, 2, 1, 6, 7, 8]
+    print(f"Attempting to sort {x}")
+    print(f"Sorted: {threaded_merge_sort(x)}")
+    x = [5, 4, 3, 2, 1, 6, 7, 8, 9]
+    print(f"Attempting to sort {x}")
+    print(f"Sorted: {threaded_merge_sort(x)}")
