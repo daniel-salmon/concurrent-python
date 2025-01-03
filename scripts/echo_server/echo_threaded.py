e060d5de6eee0d6c34ca99e923c086e9dbfffd93
daniel salmon <salmond44@gmail.com>
2025-01-03 18:00:29 -0500

    Add threaded echo server

---
+++ scripts/echo_server/echo_threaded.py
@@ -0,0 +1,55 @@
+import logging
+import socket
+import sys
+from concurrent.futures import ThreadPoolExecutor
+from socket import AF_INET, SO_REUSEADDR, SOCK_STREAM, SOL_SOCKET
+
+ADDRESS = ("127.0.0.1", 8000)
+CHUNK_SIZE = 1024
+MAX_WORKERS = 10
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(levelname)s %(asctime)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logger = logging.getLogger(__name__)
+
+logger.info(f"GIL enabled?: {sys._is_gil_enabled()}")  # type: ignore
+
+
+def main():
+    server = socket.socket(AF_INET, SOCK_STREAM)
+    server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    server.bind(ADDRESS)
+    server.listen()
+
+    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        while True:
+            connection, address = server.accept()
+            logging.info(f"Connection on {address}")
+            _ = executor.submit(handle, connection)
+
+
+def handle(connection: socket.socket) -> None:
+    try:
+        while True:
+            data = read_bytes(connection, CHUNK_SIZE)
+            connection.sendall(data)
+    except Exception as e:
+        logging.exception(e)
+    finally:
+        connection.close()
+
+
+def read_bytes(connection: socket.socket, chunk_size: int) -> bytes:
+    data = []
+    while True:
+        buffer = connection.recv(chunk_size)
+        data += [buffer]
+        if buffer[-2:] == b"\r\n":
+            return b"".join(data)
+
+
+if __name__ == "__main__":
+    main()
